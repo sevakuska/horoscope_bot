b84ba318ff6cb30ec831fc68815edb8bf441568b
Vsevolod Podtyapurin <sevakuska@outlook.com>
2022-11-22 21:31:33 +0500

    fix get_horoscope method

--- src/horoscope.py
+++ src/horoscope.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 URL = 'https://horo.mail.ru/prediction/{}/today/'
 USER_AGENT = fake_useragent.UserAgent()
 PREDICT_ATR = 'article__item article__item_alignment_left article__item_html'
+SIGN_ATR = 'hdr__inner'
 DATE_ATR = 'link__text'
 
 
@@ -16,10 +17,15 @@ async def get_horoscope(sign: str) -> str:
     }
     async with aiohttp.ClientSession() as session:
         response = await session.get(url=URL.format(sign), headers=headers)
-        text = await response.text()
-        soup = BeautifulSoup(text, 'html.parser')
-        block = soup.find('div', class_=PREDICT_ATR)
-        paragraphs = block.find_all('p')
-        date = soup.find('span', class_=DATE_ATR).text
-        prediction = '\n\n'.join([p.text for p in paragraphs])
-        return f'{date}\n\n{prediction}'
+        status = response.status
+        if status == 200:
+            text = await response.text()
+            soup = BeautifulSoup(text, 'html.parser')
+            block = soup.find('div', class_=PREDICT_ATR)
+            paragraphs = block.find_all('p')
+            sign_block = soup.find('h1', class_=SIGN_ATR).text.split(':')[-1]
+            date = soup.find('span', class_=DATE_ATR).text
+            prediction = '\n\n'.join([p.text for p in paragraphs])
+            return f'{sign_block}\{date}\n\n{prediction}'
+        else:
+            return 'Ошибка, попробуйте позже'
